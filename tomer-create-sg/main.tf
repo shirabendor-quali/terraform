9f46d2499144604ee97d0782ebb38f5e9ef9f8d0
tomer-v4 <tomer.v4@gmail.com>
2021-08-23 11:23:33 +0300

    added tomer-create-sg

---
+++ tomer-create-sg/main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = "eu-west-1"
+  access_key = var.AWS_ACCESS_KEY == "none" ? "" : var.AWS_ACCESS_KEY
+  secret_key = var.AWS_SECRET_ACCESS == "none" ? "" : var.AWS_SECRET_ACCESS
+}
+
+locals {
+  common_tags = {
+    environment  = "tintin"
+  }
+}
+
+resource "aws_security_group" "my_sg" {
+  name        = "${var.SG_NAME}"
+  description = "SG created from tomer.v service"
+
+  tags = {
+    Name = "test"
+  }
+}

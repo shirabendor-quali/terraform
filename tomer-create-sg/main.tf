d1dbe7bf4095bb3f089711157a1565dc3b5ca08d
Tomer V <48449813+tomer-v4@users.noreply.github.com>
2021-08-23 14:49:27 +0300

    Update main.tf

--- tomer-create-sg/main.tf
+++ tomer-create-sg/main.tf
@@ -16,7 +16,7 @@ resource "aws_security_group" "my_sg" {
   description = "SG created from tomer.v service"
 
   tags = {
-    Name = "test",
+    Name = "${var.SG_NAME}",
     "tomer test" = "good test"
   }
 }

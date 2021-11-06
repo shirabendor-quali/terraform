bfc253639c1d0d8533f8d033bca95ea6c89fa8ad
yakirlevi <yakir.l@quali.com>
2021-11-06 18:50:16 +0200

    added outputs

---
+++ outputs-publisher-tfv12-test/output.tf
@@ -0,0 +1,19 @@
+output "num" {
+  value = "${var.num}"
+}
+
+output "fraction" {
+  value = "${var.fraction}"
+}
+
+output "boolean" {
+  value = "${var.boolean}"
+}
+
+output tuple {
+  value = "${var.tf_tuple}"
+}
+
+output object {
+  value = "${var.tf_object}"
+}
---
+++ outputs-publisher-tfv12-test/variables.tf
@@ -0,0 +1,28 @@
+variable "num" {
+  type = number
+  default = 5
+}
+
+variable "fraction" {
+  type = number
+  default = 0.001
+}
+
+variable "boolean" {
+  type = bool
+  default = true
+}
+
+variable "tf_tuple" {
+  type = tuple([map(string),string,number])
+  default = [{key1 = "out-val1", key2 = "out-val2"}, "out-string", 6.7]
+}
+
+variable "tf_object" {
+  type = object({
+      symbol = string
+      price = number
+      levels = list(number)
+  })
+  default = {symbol = "GRVI", price = 1.2, levels = [1, 1.1, 1.4]}
+}

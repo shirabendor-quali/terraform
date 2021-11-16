61a3a5d91b79d3f51ca94d2d5db792c146867448
yakirlevi <yakir.l@quali.com>
2021-11-17 00:00:41 +0200

    added sandbox id to s3 writer hcl

--- s3-writer-hcl/main.tf
+++ s3-writer-hcl/main.tf
@@ -25,6 +25,7 @@ TF_INPUT_MAP:${var.TF_INPUT_MAP["key1"]}
 TF_INPUT_TUPLE:${var.TF_INPUT_TUPLE[0]["lmkey1"]}
 TF_INPUT_OBJECT_STR:${var.TF_INPUT_OBJECT.symbol}
 TF_INPUT_OBJECT_ELM:${var.TF_INPUT_OBJECT.levels[1]}
+SANDBOX_ID_NEW:${var.SANDBOX_ID_NEW}
 PUBLIC_ADDRESS_NEW:${var.PUBLIC_ADDRESS_NEW}
 LITERAL_PARAMETER:${var.LITERAL_PARAMETER}
 AWS_SSM_PARAMETER:${var.AWS_SSM_PARAMETER}
@@ -38,38 +39,3 @@ data "external" "presign" {
 output "s3_file_url" {
   value = "${data.external.presign.result.url}"
 }
-//
-//locals {
-//  object = <<-EOT
-//SERVER_NAME:${var.SERVER_NAME}
-//SERVER_VERSION:${var.SERVER_VERSION}
-//OUTPUTS_PUBLISHER_NO_HCL:${var.OUTPUTS_PUBLISHER_NO_HCL}
-//OUTPUTS_PUBLISHER_NUMBER:${var.OUTPUTS_PUBLISHER_NUMBER}
-//OUTPUTS_PUBLISHER_FRACTION:${var.OUTPUTS_PUBLISHER_FRACTION}
-//OUTPUTS_PUBLISHER_BOOL:${var.OUTPUTS_PUBLISHER_BOOL}
-//OUTPUTS_PUBLISHER_TUPLE_KEY2:${var.OUTPUTS_PUBLISHER_TUPLE[0]["key2"]}
-//OUTPUTS_PUBLISHER_TUPLE_NUM:${var.OUTPUTS_PUBLISHER_TUPLE[2]}
-//OUTPUTS_PUBLISHER_OBJECT_ELM:${var.OUTPUTS_PUBLISHER_OBJECT.symbol}
-//TF_INPUT_STRING:${var.TF_INPUT_STRING}
-//TF_INPUT_NUMBER:${var.TF_INPUT_NUMBER}
-//TF_INPUT_LIST:${var.TF_INPUT_LIST[0]}
-//TF_INPUT_MAP:${var.TF_INPUT_MAP["key1"]}
-//TF_INPUT_TUPLE:${var.TF_INPUT_TUPLE[0]["lmkey1"]}
-//TF_INPUT_OBJECT_STR:${var.TF_INPUT_OBJECT.symbol}
-//TF_INPUT_OBJECT_ELM:${var.TF_INPUT_OBJECT.levels[1]}
-//PUBLIC_ADDRESS_NEW:${var.PUBLIC_ADDRESS_NEW}
-//LITERAL_PARAMETER:${var.LITERAL_PARAMETER}
-//AWS_SSM_PARAMETER:${var.AWS_SSM_PARAMETER}
-//EOT
-//}
-//
-//resource "local_file" "object" {
-//  filename = "object-output"
-//  content = local.object
-//}
-//
-//locals {
-//  output_object = {symbol = "GRVI", price = 1.2, levels = [1, 1.1, 1.4]}
-//  output_tuple =  [{key1 = "out-val1", key2 = "out-val2"}, "out-string", 6.7]
-//}
-//

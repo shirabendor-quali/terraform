10801527480535a24e34145bef9fbbd66a336f43
shirabendor-quali <76521367+shirabendor-quali@users.noreply.github.com>
2022-06-14 13:53:40 +0300

    Update main.tf

--- s3-write/main.tf
+++ s3-write/main.tf
@@ -6,4 +6,5 @@ resource "aws_s3_bucket_object" "object" {
   bucket  = "${var.BUCKET_NAME}"
   key     = "${var.SANDBOX_ID}.json"
   content = "{ \"Content\": \"${var.CONTENT}\"}"
+  acl     = "bucket-owner-full-control"
 }

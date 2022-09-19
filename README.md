fabadc88142425450c77f16a2dd31d7b3600464a
shirabendor-quali <shira.b@quali.com>
2022-09-19 14:28:35 +0300

    tfvars

--- README.md
+++
@@ -1,6 +0,0 @@
-# terraform playground
-
-## s3store & s3reader
-
-- s3store terraform module which creates a file on s3 bucket
-- s3reader is a python server which reads file's content
---
+++ s3-write-tfvars/main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_s3_bucket_object" "object" {
+  bucket  = "${var.BUCKET_NAME}"
+  key     = "${var.SANDBOX_ID}.json"
+  content = "{ \"Content\": \"${var.CONTENT}\"}"
+  acl     = "bucket-owner-full-control"
+}

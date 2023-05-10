08dee8ab5c59df4459b158db18bbde619a2e7ae3
shirabendor-quali <76521367+shirabendor-quali@users.noreply.github.com>
2023-05-10 14:54:17 +0300

    Update outputs.tf

--- ebs/outputs.tf
+++ ebs/outputs.tf
@@ -2,8 +2,3 @@ output "environment_url" {
   value = aws_elastic_beanstalk_environment.environment.endpoint_url
 }
 
-output "application_version" {
-  value = aws_elastic_beanstalk_application_version.default.name
-}
-
-

c345fbf7a3b772d28bb0d6f2908252f2054a821f
shirabendor-quali <76521367+shirabendor-quali@users.noreply.github.com>
2023-05-24 12:02:47 +0300

    Update variables.tf

--- s3-create/variables.tf
+++ s3-create/variables.tf
@@ -3,3 +3,9 @@ variable "region" {
     type = string
     default = "eu-west-1"
 }
+
+variable "region2" {
+    description = "Region where to create resources" 
+    type = string
+    default = null
+}

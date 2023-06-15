7b631227caa3e785244cbf65bf9bb5409e26f09f
shirabendor-quali <76521367+shirabendor-quali@users.noreply.github.com>
2023-06-15 11:59:40 +0300

    Update main.tf

--- helloworld/main.tf
+++ helloworld/main.tf
@@ -1,20 +1,7 @@
-variable "input_value" {
-  type        = string
-  description = "Input value for the Terraform configuration"
-  default     = "default"
-}
+package torque.environment
 
-resource "null_resource" "example" {
-  triggers = {
-    input_value = var.input_value
-  }
+import future.keywords.if
 
-  provisioner "local-exec" {
-    command = <<EOT
-if [ "${var.input_value}" == "shira" ]; then
-  echo "Error: Input value is 'shira'. Failing apply phase."
-  exit 1
-fi
-EOT
-  }
+result := { "decision": "Manual" } if {
+  input.action_identifier.action_type != "Extend"  
 }

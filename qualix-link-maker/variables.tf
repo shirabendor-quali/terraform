f838818a80bcc2ff56392807b49ddc970248a4e6
shirabendor-quali <76521367+shirabendor-quali@users.noreply.github.com>
2023-11-28 21:11:27 +0200

    Update variables.tf

--- qualix-link-maker/variables.tf
+++ qualix-link-maker/variables.tf
@@ -2,3 +2,14 @@ variable "qualix_ip" {
   type = string
   default = "127.0.0.1"  
 }
+
+variable "target_username" {
+  type = string
+  default = "ubuntu"
+}
+
+variable "target_password" {
+  type = string
+  default = "P@$$w0rd1234!"
+}
+

706f76a4c4330168a16512b1fd2692e3edecec69
shirabendor-quali <76521367+shirabendor-quali@users.noreply.github.com>
2023-11-29 09:33:49 +0200

    Update main.tf

--- qualix-link-maker/main.tf
+++ qualix-link-maker/main.tf
@@ -1,33 +1,4 @@
-variable "qualix_ip" {
-    type = string
-    #validation {
-    #regex(...) fails if it cannot find a match
-    #condition = can(regex("[0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+", var.qualix_ip))
-    #error_message = "QualiX IP Address must be a valid IP"
-    #}
-}
-
-variable "protocol" {
-  type = string
-  #validation {
-  #  condition = var.protocol == "rdp" || var.protocol == "ssh"
-  #  error_message = "Invalid protocol type, only 'rdp' and 'ssh' are currently supported"
-  #}
-}
-
-variable "connection_port" {
-  type = number
-}
-
 variable "target_ip_address" {
-  type = string
-}
-
-variable "target_username" {
-  type = string
-}
-
-variable "target_password" {
   type = string
 }
 

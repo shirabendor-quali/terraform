5436d480e4a5687a6ab7f96d0cd6b79fa887c8b2
shirabendor-quali <76521367+shirabendor-quali@users.noreply.github.com>
2024-02-11 18:43:28 +0200

    Update power-on-vm.sh

--- scripts/power-on-vm.sh
+++ scripts/power-on-vm.sh
@@ -1,4 +1,6 @@
+export ACCESS_TOKEN=$(curl -H "Metadata: true" "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https://management.azure.com/" | jq -r .access_token)
+
 # Obtain the federated token
-federated_token=$(cat $AZURE_FEDERATED_TOKEN_FILE)
+#federated_token=$(cat $AZURE_FEDERATED_TOKEN_FILE)
 # Use the federated token to authenticate your API call
-curl -X POST -H "Authorization: Bearer $federated_token" -H "Content-Type: application/json" https://management.azure.com/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Compute/virtualMachines/{vmName}/start?api-version=2023-09-01
+curl -X POST -H "Authorization: Bearer $ACCESS_TOKEN" -H "Content-Type: application/json" https://management.azure.com/subscriptions/$ARM_SUBSCRIPTION_ID/resourceGroups/$1/providers/Microsoft.Compute/virtualMachines/$2/start?api-version=2023-09-01

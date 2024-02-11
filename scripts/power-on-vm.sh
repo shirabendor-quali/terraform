48e4702faa2ec228f2a4f1e3014ed0d93825fa39
shirabendor-quali <76521367+shirabendor-quali@users.noreply.github.com>
2024-02-11 18:55:40 +0200

    Update power-on-vm.sh

--- scripts/power-on-vm.sh
+++ scripts/power-on-vm.sh
@@ -1,4 +1,4 @@
-export ACCESS_TOKEN=$(curl -H "Metadata: true" "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https://management.azure.com/" | jq -r .access_token)
+export ACCESS_TOKEN=$(curl -H "Metadata: true" "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https://management.azure.com/" --header "Content-Length: 0" | jq -r .access_token)
 
 # Obtain the federated token
 #federated_token=$(cat $AZURE_FEDERATED_TOKEN_FILE)

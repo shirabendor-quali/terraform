fbe0e8aacc530c6e3790ab301898f2020efa625d
shirabendor-quali <76521367+shirabendor-quali@users.noreply.github.com>
2024-02-12 17:12:13 +0200

    Create variables.tf

---
+++ azure_vm_legacy_wi/variables.tf
@@ -0,0 +1,11 @@
+variable "vm_name" {
+  type = string
+  default = "shiravm"
+  description = "vm Name"
+}
+
+variable "resource_group" {
+  type = string
+  default = "shirarg"
+  description = "resource group name"
+}

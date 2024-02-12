d118c7e04f924835a4282426c1a3a82f6e813d6d
shirabendor-quali <76521367+shirabendor-quali@users.noreply.github.com>
2024-02-12 15:46:20 +0200

    Update power-on-vm.sh

--- scripts/power-on-vm.sh
+++ scripts/power-on-vm.sh
@@ -1,6 +1,7 @@
-export ACCESS_TOKEN=$(curl -H "Metadata: true" "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https://management.azure.com/" --header "Content-Length: 0" | jq -r .access_token)
+#export ACCESS_TOKEN=$(curl -H "Metadata: true" "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https://management.azure.com/" --header "Content-Length: 0" | jq -r .access_token)
 
 # Obtain the federated token
-#federated_token=$(cat $AZURE_FEDERATED_TOKEN_FILE)
+federated_token=$(cat $AZURE_FEDERATED_TOKEN_FILE)
 # Use the federated token to authenticate your API call
-curl -X POST -H "Authorization: Bearer $ACCESS_TOKEN" -H "Content-Type: application/json" --header "Content-Length: 0" https://management.azure.com/subscriptions/$ARM_SUBSCRIPTION_ID/resourceGroups/$1/providers/Microsoft.Compute/virtualMachines/$2/start?api-version=2023-09-01 
+#curl -X POST -H "Authorization: Bearer $ACCESS_TOKEN" -H "Content-Type: application/json" --header "Content-Length: 0" https://management.azure.com/subscriptions/$ARM_SUBSCRIPTION_ID/resourceGroups/$1/providers/Microsoft.Compute/virtualMachines/$2/start?api-version=2023-09-01 
+curl -X POST -H "Authorization: Bearer $federated_token" -H "Content-Type: application/json" https://management.azure.com/subscriptions/$ARM_SUBSCRIPTION_ID/resourceGroups/$1/providers/Microsoft.Compute/virtualMachines/$2/start?api-version=2023-09-01

eb12e7988f46d5a7bbf42347e11d583245991263
shirabendor-quali <76521367+shirabendor-quali@users.noreply.github.com>
2024-02-13 11:49:17 +0200

    Update power-on-vm.sh

--- scripts/power-on-vm.sh
+++ scripts/power-on-vm.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 # get access token
-token_response=$(curl -s 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fmanagement.azure.com%2F&client_id='$ARM_CLIENT_ID -H Metadata:true)
+token_response=$(curl -s 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fmanagement.azure.com%2F&client_id='$AZURE_CLIENT_ID -H Metadata:true)
 token=$(echo $token_response | grep -oP '(?<="access_token":")[^"]*')
+echo "token"
+echo $token
+echo "client1"
+echo $AZURE_CLIENT_ID
+echo "client2"
+echo $ARM_CLIENT_ID
 curl -X POST -H "Authorization: Bearer $token" -H "Content-Type: application/json" --header "Content-Length: 0" https://management.azure.com/subscriptions/$ARM_SUBSCRIPTION_ID/resourceGroups/$1/providers/Microsoft.Compute/virtualMachines/$2/start?api-version=2023-09-01
 
 # generate sas
